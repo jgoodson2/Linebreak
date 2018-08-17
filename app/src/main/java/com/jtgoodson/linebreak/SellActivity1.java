132686bb784b57374846788fae0f3dc8c2d11a6f
Jonathon Goodson <goodson.jonathon@gmail.com>
2018-08-16 21:38:46 -0400

    Initial commit

---
+++ app/src/main/java/com/jtgoodson/linebreak/SellActivity1.java
@@ -0,0 +1,13 @@
+package com.jtgoodson.linebreak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SellActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sell1);
+    }
+}
